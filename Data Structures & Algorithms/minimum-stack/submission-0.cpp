0ce8b96eadfd5ef535f9bb5092b475d1246515b4
Bui-Minh-Quan <160519518+Bui-Minh-Quan@users.noreply.github.com>
2026-07-26 10:47:31 +0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+private:
+    stack<pair<int, int>> st;
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if (st.empty()) {
+            st.push({val, val});
+            return;
+        }
+        
+        int min_val = min(val, st.top().second);
+        st.push({val, min_val});
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
